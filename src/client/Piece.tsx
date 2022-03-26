5f07088a84dfa1365e3d96c3e6a3731465b41383
Louis-Antoine DE MOULINS DE ROCHEFORT <lde-moul@student.42.fr>
2022-03-26 18:59:40 +0100

    Rewrite spawnNextPiece without Immer

--- src/client/Piece.tsx
+++ src/client/Piece.tsx
@@ -1,16 +1,17 @@
  'use strict';
 
+import Player from "./Player";
 import Vector2D from "./Vector2D";
-
-import produce from "immer";
 
 export default interface Piece {
   blocks: Vector2D[];
   center: Vector2D;
 };
 
-export const spawnNextPiece = (player: Player) => {
-  return produce(player, draft => {
-    draft.piece = draft.pieceQueue.shift();
-  });
-};
+export const spawnNextPiece = (player: Player) =>
+  {
+    const queue = [...player.pieceQueue];
+    const piece = queue.shift();
+
+    return { piece, queue, ...player };
+  };
