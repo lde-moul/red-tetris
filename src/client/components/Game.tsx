512342b5d0bfdbb1c90e7da1e187a54d82632685
Louis-Antoine DE MOULINS DE ROCHEFORT <lde-moul@student.42.fr>
2022-03-26 18:59:42 +0100

    Implement gravity

--- src/client/components/Game.tsx
+++ src/client/components/Game.tsx
@@ -2,9 +2,11 @@
 
 import Block from './Block';
 import handleGameKeyDown from '../handleGameKeyDown';
+import { movePiece } from '../Piece';
 import { useTracked } from '../state';
 import '../../../styles.css';
 
+import produce from 'immer';
 import React, { useEffect } from 'react';
 
 export default () => {
@@ -13,11 +15,23 @@ export default () => {
   const handleKeyDown = (event: KeyboardEvent) =>
     setState(prev => handleGameKeyDown(event, prev));
 
+  const handleTick = () => {
+    setState(prev => produce(prev, draft => {
+      const playerId = draft.room.players.findIndex(player => player.name == draft.playerName);
+      const player = draft.room.players[playerId];
+
+      if (player.piece)
+        draft.room.players[playerId] = movePiece(player, { x: 0, y: 1 }, prev.socket);
+    }));
+  };
+
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
+    const ticker = setInterval(handleTick, 500);
 
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
+      clearInterval(ticker);
     };
   }, []);
 
