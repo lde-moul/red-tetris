9788b2d2730f8121d1d54e04dbad41fee50bc535
Louis-Antoine DE MOULINS DE ROCHEFORT <lde-moul@student.42.fr>
2022-03-26 18:59:41 +0100

    Update tests involving shapes

--- test/client/Piece.tsx
+++ test/client/Piece.tsx
@@ -1,15 +1,18 @@
 'use strict';
 
-import { canPieceBeHere, rotatePiece, translatePiece } from '../../src/client/Piece';
+import Piece, { canPieceBeHere, rotatePiece, translatePiece } from '../../src/client/Piece';
 import { attachPieceToBoard, getEmptyBoard } from '../../src/client/Player';
 import shapes from '../../src/server/shapes';
 
 import assert from 'assert';
 
 describe('Piece', function() {
-  it('should move the piece down by 3 blocks', () => {
-    const piece = { ...shapes[1] };
+  const getShape = (id: number): Piece => ({
+    blocks: shapes[id].blocks,
+    center: shapes[id].center
+  });
 
+  it('should move the piece down by 3 blocks', () => {
     const expected = {
       blocks: [
         { x: 0.5, y: 3.5 },
@@ -20,12 +23,12 @@ describe('Piece', function() {
       center: { x: 1.5, y: 4.5 }
     };
 
-    const movedPiece = translatePiece(piece, { x: 0, y: 3 });
+    const movedPiece = translatePiece(getShape(1), { x: 0, y: 3 });
     assert.deepStrictEqual(movedPiece, expected);
   });
 
   it('should rotate the piece', () => {
-    const piece = { ...shapes[5] };
+    const piece = getShape(5);
 
     const expected = {
       ...piece,
@@ -41,16 +44,16 @@ describe('Piece', function() {
   });
 
   it("should make the piece collide when it's inside the board", () => {
-    assert(canPieceBeHere(shapes[3], getEmptyBoard()));
+    assert(canPieceBeHere(getShape(3), getEmptyBoard()));
   });
 
   it("should make the piece collide when it's is outside the board", () => {
-    const piece = translatePiece(shapes[3], { x: -1, y: 0 });
+    const piece = translatePiece(getShape(3), { x: -1, y: 0 });
     assert(!canPieceBeHere(piece, getEmptyBoard()));
   });
 
   it("should make the piece collide if the board contains filled blocks at its position", () => {
-    const piece = shapes[3];
+    const piece = getShape(3);
     const board = attachPieceToBoard(piece, getEmptyBoard());
     assert(!canPieceBeHere(piece, board));
   });
